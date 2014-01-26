ae372d3a4c8c88603a0172062ce8ba24f67990ed
Petre Rodan <petre.rodan@simplex.ro>
2014-01-26 13:15:14 +0200

    add flash r/w driver

--- firmware/proj.c
+++ firmware/proj.c
@@ -14,25 +14,31 @@
 #include "drivers/serial_bitbang.h"
 #include "drivers/ads1110.h"
 #include "drivers/adc.h"
-
-// an integer representing the time after which the uC will disable the latch
-// standby_time(sec) = standby_time(int) * 2sec
-// (2sec is the overflow time of TA0R)
-uint16_t standby_time;
+#include "drivers/flash.h"
 
 int16_t tue; // ticks until end
 
 volatile uint8_t port1_last_event;
 struct ads1110 eadc;
 
+struct settings_t s;
+
 char str_temp[83];
 
 void display_menu(void)
 {
-    uart1_tx_str("\r\nuCurrent controller\r\n  time until poweroff: ", 47);
-    sprintf(str_temp, "%d sec\r\n ADS1110 status:\r\n  config:     0x%02x\r\n  conversion: %s%01d.%04dV\r\n", tue*2, eadc.config, 
-            eadc.conv < 0 ? "-": "", abs((int16_t) eadc.conv / 10000), abs((int16_t) eadc.conv % 10000) );
+    sprintf(str_temp, "\r\n --- uCurrent controller ---\r\n  time until poweroff: %d sec\r\n", tue*2);
     uart1_tx_str(str_temp, strlen(str_temp));
+
+    sprintf(str_temp, "  config ver:%d [e]adc:%d [a]dc:%d s_[t]:%d s_[u]:%d\r\n", s.ver, s.enable_eadc, s.enable_adc, s.standby_time, s.standby_unused);
+    uart1_tx_str(str_temp, strlen(str_temp));
+
+    sprintf(str_temp, " ADS1110 status:\r\n  config:     0x%02x\r\n  conversion: %s%01d.%04dV\r\n", eadc.config, eadc.conv < 0 ? "-": "", abs((int16_t) eadc.conv / 10000), abs((int16_t) eadc.conv % 10000) );
+    uart1_tx_str(str_temp, strlen(str_temp));
+
+    sprintf(str_temp, "input [eatu]: ");
+    uart1_tx_str(str_temp, strlen(str_temp));
+
 }
 
 static void timer_a0_irq(enum sys_message msg)
@@ -57,7 +63,7 @@ static void port1_irq(enum sys_message msg)
     if ((P1IN & TRIG1) == 0) {
         return;
     } else {
-        tue = standby_time;
+        tue = s.standby_time;
         display_menu();
     }
 }
@@ -66,9 +72,9 @@ int main(void)
 {
     main_init();
     uart1_init();
-    standby_time = 900;         // by default shut down after 30minutes
-    // read from flash
-    tue = standby_time;
+    settings_init();
+
+    tue = s.standby_time;
     latch_enable;
     uc_enable;
     led_on;
@@ -77,6 +83,8 @@ int main(void)
 
     sys_messagebus_register(&timer_a0_irq, SYS_MSG_TIMER0_IFG);
     sys_messagebus_register(&port1_irq, SYS_MSG_P1IFG);
+
+    display_menu();
 
     while (1) {
         // sleep
@@ -139,6 +147,29 @@ void main_init(void)
     USBKEYPID = 0x9600;
 
     timer_a0_init();
+}
+
+void settings_init(void)
+{
+    uint8_t *src_p, *dst_p;
+    uint8_t i;
+
+    src_p = FLASH_ADDR;
+    dst_p = (uint8_t *) & s;
+
+    // check if flash contains settings for current firmware version
+    if ((*src_p) != VERSION) {
+        // read defaults
+        src_p = (uint8_t *) & defaults;
+        // save defaults to the flash
+        flash_save(FLASH_ADDR, (void *) &defaults, sizeof(s));
+    }
+
+    for (i = 0; i < sizeof(s); i++) {
+        *dst_p++ = *src_p++;
+    }
+
+
 }
 
 /*
--- firmware/proj.h
+++ firmware/proj.h
@@ -22,11 +22,34 @@
 #define led_on              P2OUT |= BIT0
 #define led_off             P2OUT &= ~BIT0
 
+#define VERSION             1   // must be incremented if struct settings changes
+#define FLASH_ADDR          SEGMENT_B
+
 #define true                1
 #define false               0
 
 void main_init(void);
+void settings_init(void);
 void wake_up(void);
 void check_events(void);
+
+struct settings_t {
+    uint8_t ver;                // firmware version
+    int8_t enable_eadc;         // enable external adc. 0 false, 1 true, -1 false but ADS1110 present
+    uint8_t enable_adc;         // enable internal adc for battery check
+    uint16_t standby_time;      // number of TA0 overflows after which latch is disabled
+                                // (1 overflow takes 2 seconds)
+    uint16_t standby_unused;    // innactivity timeout after which latch is disabled
+                                // enable_adc must be 1 for this timer to be enabled
+};
+
+static const struct settings_t defaults = {
+    VERSION,    // ver
+    1,          // enable_eadc
+    0,          // enable_adc
+    900,        // standby_time
+    600,        // standby_unused
+};
+
 
 #endif
